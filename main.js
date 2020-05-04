ad7c9fbd0369a13f2ba52b033578edbb6ed805a0
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-04 18:59:03 +0200

    Esercitazione Slider Automatico Completa

--- main.js
+++ main.js
@@ -4,35 +4,70 @@
 $('.next').click(function () {
     // intercettare l'immagine con la classe .visible
     var img_corrente = $('img.visible');
-    console.log(img_corrente);
     // rimuovo la classe .visible all'img corrente
     img_corrente.removeClass('visible');
-    // intercettarel'immagine successiva
+    // intercettare l'immagine successiva
     var img_successiva = img_corrente.next('img');
     // aggiungo la classe .visible a img_successiva
+    // Se img_successiva è l'ultima si ricomincia da capo
     if (img_successiva.length != 0) {
         img_successiva.addClass('visible');
     } else {
-        // Se img_successiva è l'ultima si ricomincia da capo
         img_successiva = $('img:first-child');
         img_successiva.addClass('visible');
     }
 })
 
+// intercettare il click sulla classe .prev
 $('.prev').click(function () {
     // intercettare l'immagine con la classe .visible
     var img_corrente = $('img.visible');
-    console.log(img_corrente);
     // rimuovo la classe .visible all'img corrente
     img_corrente.removeClass('visible');
-    // intercettarel'immagine successiva
+    // intercettare l'immagine successiva
     var img_precedente = img_corrente.prev('img');
     // aggiungo la classe .visible a img_successiva
+    // Se img_successiva è l'ultima si ricomincia da capo
     if (img_precedente.length != 0) {
         img_precedente.addClass('visible');
     } else {
-        // Se img_successiva è l'ultima si ricomincia da capo
         img_precedente = $('img:last-child');
         img_precedente.addClass('visible');
     }
 })
+
+// Ogni 3 secondi le slide si alternano
+
+var clock = setInterval(function(){
+    var img_corrente = $('.visible');
+    img_corrente.removeClass('visible');
+    img_successiva = img_corrente.next('img');
+
+    if (img_successiva.length != 0) {
+        img_successiva.addClass('visible');
+    } else {
+        img_successiva = $('img:first-child');
+        img_successiva.addClass('visible');
+    }
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END

0305c23ad89e2e0b76dcc28f0c340cf89f61899c
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-08-11 13:12:04 +0200

    Aggiornamento + Ottimizzazione: Add Bullet bottom

--- main.js
+++ main.js
@@ -4,14 +4,24 @@
 function slideSwitch(){
 
     var img_corrente = $('.visible');
+    // recupero il pallino corrente
+    var pallino_corrente = $('.fa-circle.visible');
+
     img_corrente.removeClass('visible');
+    // tolgo la classe active al pallino corrente
+    pallino_corrente.removeClass('visible');
+
     img_successiva = img_corrente.next('img');
+    // recupero il pallino successivo
+    var pallino_successivo = pallino_corrente.next('.fa-circle');
 
     if (img_successiva.length != 0) {
         img_successiva.addClass('visible');
+        pallino_successivo.addClass("visible");
     } else {
         img_successiva = $('img:first-child');
         img_successiva.addClass('visible');
+        $(".fa-circle:first-child").addClass("visible");
     }
 };
 
@@ -44,17 +54,25 @@ $(function () {
 $('.next').click(function () {
     // intercettare l'immagine con la classe .visible
     var img_corrente = $('img.visible');
+    var pallino_corrente = $(".fa-circle.visible");
+
     // rimuovo la classe .visible all'img corrente
     img_corrente.removeClass('visible');
+    pallino_corrente.removeClass("visible");
+
     // intercettare l'immagine successiva
     var img_successiva = img_corrente.next('img');
+    var pallino_successivo = pallino_corrente.next(".fa-circle");
+
     // aggiungo la classe .visible a img_successiva
     // Se img_successiva è l'ultima si ricomincia da capo
     if (img_successiva.length != 0) {
         img_successiva.addClass('visible');
+        pallino_successivo.addClass("visible");
     } else {
         img_successiva = $('img:first-child');
         img_successiva.addClass('visible');
+        $(".fa-circle:first-child").addClass("visible");
     }
 })
 
@@ -62,17 +80,24 @@ $('.next').click(function () {
 $('.prev').click(function () {
     // intercettare l'immagine con la classe .visible
     var img_corrente = $('img.visible');
+    var pallino_corrente = $(".fa-circle.visible");
+
     // rimuovo la classe .visible all'img corrente
     img_corrente.removeClass('visible');
+    pallino_corrente.removeClass("visible");
+
     // intercettare l'immagine successiva
     var img_precedente = img_corrente.prev('img');
+    var pallino_successivo = pallino_corrente.prev(".fa-circle");
     // aggiungo la classe .visible a img_successiva
     // Se img_successiva è l'ultima si ricomincia da capo
     if (img_precedente.length != 0) {
         img_precedente.addClass('visible');
+        pallino_successivo.addClass("visible");
     } else {
-        img_precedente = $('img:last-child');
+        img_precedente = $("img:last-of-type");
         img_precedente.addClass('visible');
+        $(".fa-circle:last-child").addClass("visible");
     }
 })
 
